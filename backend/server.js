8ff0405c9000aa6c50063e8d5a3c3266eee1e87a
melkie yilk <melkieyilk@gmail.com>
2025-04-30 21:50:53 +0300

    import the router to the main server app and implement it

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import UserRouter from "./routes/user.routes.js";
+import ItemRouter from "./routes/item.routes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -20,13 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
-app.use(cookieParser()); 
+app.use(cookieParser());
 
 app.get("/api", (_, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/users", UserRouter);
+app.use("/api/items", ItemRouter);
 
 app.use(notFound);
 app.use(errorHandler);
