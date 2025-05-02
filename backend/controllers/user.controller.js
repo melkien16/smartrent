854b4dec655db1fc39326a0034830e25c0c38542
melkie yilk <melkieyilk@gmail.com>
2025-05-02 14:38:21 +0300

    finish the users controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
 // @desc     Auth user & get token
-// @route  POST /api/users/login
+// @route  POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -17,7 +17,9 @@ const authUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      address: user.address,
       isAdmin: user.isAdmin,
+      avatar: user.avatar,
     });
   } else {
     res.status(401);
@@ -25,7 +27,7 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc     user && get token
+// @desc     Register user
 // @route  POST /api/users/login
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
@@ -65,7 +67,6 @@ const registerUser = asyncHandler(async (req, res) => {
 // @desc    Logout user / clear cookie
 // @route  POST /api/users/logout
 // @access  Private
-
 const logoutUser = asyncHandler(async (req, res) => {
   res.cookie("jwt", "", {
     httpOnly: true,
@@ -106,6 +107,9 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
+    user.phone = req.body.phone || user.phone;
+    user.address = req.body.address || user.address;
+    user.avatar = req.body.avatar || user.avatar;
 
     if (req.body.password) {
       user.password = req.body.password;
@@ -133,28 +137,73 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route  GET /api/users
 // @access  Private/Admin
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const users = await User.find({});
+  if (!users) {
+    res.status(404);
+    throw new Error("No users found");
+  }
+  res.status(200).json(users);
 });
 
 // @desc    Get user by id
 // @route  GET /api/users/:id
 // @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Delete user
 // @route  DELETE /api/users/:id
 // @access  Private/Admin
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.status(200).json({ message: "User removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc    Update user
 // @route  PUT /api/users/:id
 // @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("auth user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.phone = req.body.phone || user.phone;
+    user.address = req.body.address || user.address;
+    user.isAdmin = req.body.isAdmin ?? user.isAdmin;
+    user.isPremium = req.body.isPremium ?? user.isPremium;
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      phone: updatedUser.phone,
+      address: updatedUser.address,
+      avatar: updatedUser.avatar,
+      isPremium: updatedUser.isPremium,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
