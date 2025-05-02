62a0557baaff1dd2ba345bad8d531fa1733175d5
melkie yilk <melkieyilk@gmail.com>
2025-05-02 17:02:58 +0300

    add the base url into the itemfetcher

--- frontend/src/itemFetcher.js
+++ frontend/src/itemFetcher.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import BASE_URL from '../constants/baseUrl';
 
 // Function to fetch items from the backend using Axios
 async function  fetchItems() {
-    return axios.get('http://localhost:5000/api/items')
+    return axios.get(`${BASE_URL}/items`)
         .then((res) => {
             const items = res.data;
             console.log('Fetched items:', items);
