20d045379ddf937a7a9d3724dc927210edb1a56d
melkie yilk <melkieyilk@gmail.com>
2025-05-02 18:49:40 +0300

    createa a dummy category

---
+++ backend/data/category.js
@@ -0,0 +1,32 @@
+const categories = [
+  {
+    id: "electronics",
+    name: "Electronics",
+    count: 156,
+    image:
+      "https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: "outdoor",
+    name: "Outdoor Gear",
+    count: 98,
+    image:
+      "https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: "tools",
+    name: "Tools",
+    count: 87,
+    image:
+      "https://images.pexels.com/photos/1409215/pexels-photo-1409215.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: "sports",
+    name: "Sports Equipment",
+    count: 112,
+    image:
+      "https://images.pexels.com/photos/2607072/pexels-photo-2607072.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+];
+
+export default categories;
