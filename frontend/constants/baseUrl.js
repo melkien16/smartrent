feb56a9955e1c864d6a5cc0820dffa8052b6a673
melkie yilk <melkieyilk@gmail.com>
2025-05-04 00:16:37 +0300

    modify final constants

--- frontend/constants/baseUrl.js
+++ frontend/constants/baseUrl.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://smartrent-12.onrender.com";
+const BASE_URL = "https://smartrent-12.onrender.com/api";
 
 export default BASE_URL;
--- frontend/src/constants/baseUrl.js
+++ frontend/src/constants/baseUrl.js
@@ -1,4 +1,4 @@
 // Base URL for API calls
-const BASE_URL = "https://smartrent-12.onrender.com";
+const BASE_URL = "https://smartrent-12.onrender.com/api";
 
 export default BASE_URL;
