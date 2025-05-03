b48202495cadfabc699702e9efb478257d734e33
melkie yilk <melkieyilk@gmail.com>
2025-05-04 00:59:58 +0300

    add some adjustments

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,8 +9,8 @@ const generateToken = (res, userId) => {
   // Set jwt as HTTP-only cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "Strict",
+    secure: true,
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
