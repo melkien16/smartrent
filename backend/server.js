e3e2440b865463349f8c2ccc0e6c43e4a7e9fc9b
melkie yilk <melkieyilk@gmail.com>
2025-05-04 00:20:32 +0300

    finilize modifications

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "process.env.CLIENT_URL",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
--- frontend/src/constants/baseUrl.js
+++ frontend/src/constants/baseUrl.js
@@ -1,4 +1,3 @@
-// Base URL for API calls
 const BASE_URL = "https://smartrent-12.onrender.com/api";
 
 export default BASE_URL;
