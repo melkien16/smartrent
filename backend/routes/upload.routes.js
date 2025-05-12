bb3009bba1fb77d64c14a02ff3c9c5dfea39f0e0
melkie yilk <melkieyilk@gmail.com>
2025-05-12 21:56:28 +0300

    change the locall upload to cloudinary storege

--- backend/routes/upload.routes.js
+++ backend/routes/upload.routes.js
@@ -1,48 +1,25 @@
-import path from 'path';
-import express from 'express';
-import multer from 'multer';
+import express from "express";
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    );
+// Cloudinary storage config
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "rentsmart",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
-function fileFilter(req, file, cb) {
-  const filetypes = /jpe?g|png|webp/;
-  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
+const upload = multer({ storage });
 
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = mimetypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    cb(null, true);
-  } else {
-    cb(new Error('Images only!'), false);
-  }
-}
-
-const upload = multer({ storage, fileFilter });
-const uploadSingleImage = upload.single('image');
-
-router.post('/', (req, res) => {
-  uploadSingleImage(req, res, function (err) {
-    if (err) {
-      return res.status(400).send({ message: err.message });
-    }
-
-    res.status(200).send({
-      message: 'Image uploaded successfully',
-      image: `/${req.file.path}`,
-    });
+router.post("/", upload.single("image"), (req, res) => {
+  res.status(200).send({
+    message: "Image uploaded to Cloudinary successfully",
+    image: req.file.path,
   });
 });
 
